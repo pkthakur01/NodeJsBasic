f5dc66308eafdd2782282b7e822343d295b8e763
pkthakur01 <prakashpusa#gmail.com>
2019-11-20 16:04:25 +0530

    added code in login

--- Project/nodelogin/register.js
+++ Project/nodelogin/register.js
@@ -21,44 +21,18 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
-});
-app.get('/branch', function(request, response) {
 	response.sendFile(path.join(__dirname + '/register.html'));
 });
-
 app.get('/login', function(request, response) {
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
+
 
 app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
 		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
-id = 2;
-app.post('/reg', function(request, response) {
-    id = id+1;
-	var username = request.body.username;
-    var password = request.body.password;
-    var password = request.body.email;
-    var sql = "INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (id, username, password, email)";
-        
-		connection.query(' {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
